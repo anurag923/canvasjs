a2dbb66f5407c813997325d5b316ea9aaf555892
anurag <anuraganalogit@gmail.com>
2021-10-04 12:05:03 +0530

    2021-10-04

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+declare const CanvasJS: any;
+import * as $ from 'jquery';
+import { webSocket } from "rxjs/webSocket";
+const subject = webSocket("wss://socket.polygon.io/crypto");
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit{
+  livedata:any;
+  datapoints = []
+  yval:any;
+  constructor(private http:HttpClient,private datePipe: DatePipe) {}
+  ngOnInit(){
+	
+    subject.next({action:"auth",params:"6sEFcNe2upitHW5lt9dp7EfkIuxoR58k"});
+    subject.subscribe(
+      (msg) => {console.log('message received: ' + msg);
+      this.livedata = msg;
+	  for(var i=0;i<this.livedata.length;i++){
+		  if("p" in this.livedata[i]){
+			var formattedTime = this.datePipe.transform(this.livedata[0].t,'HH:mm:ss');
+			this.datapoints.push({y:this.livedata[0].p,label:formattedTime});
+			console.log('datapoints',this.datapoints);
+			
+		  }
+	  }
+	  for(var j=0;j<this.datapoints.length;j++){
+		  if(this.datapoints[j].label == "11:56:00"){
+			  this.yval = this.datapoints[j].y;
+		  }
+	  }
+		let chart = new CanvasJS.Chart("chartContainer1",{
+			exportEnabled: true,
+			title:{
+				text:"BTC-USD"
+			},
+			axisY:{
+				stripLines:[
+					 {
+						 
+						 value:this.yval,               
+						 color:"red",
+						 thickness:2
+					 }
+					 ]
+		  	},
+			data: [{
+				type: "line",
+				color:"green",
+				dataPoints : this.datapoints,
+			}]
+		});
+		console.log('chart',chart);
+		chart.render();
+		
+		
+    }, // Called whenever there is a message from the server.
+      (err) => {console.log(err)}, // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+    subject.next({action:"subscribe",params:"XT.BTC-USD"});
+	
+}
 }
