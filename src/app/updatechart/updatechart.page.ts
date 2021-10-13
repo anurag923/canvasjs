248eb5bc4d8642e77f6d547c1229eb34f5b26e60
anurag <anuraganalogit@gmail.com>
2021-10-13 19:06:16 +0530

    anurag

--- src/app/updatechart/updatechart.page.ts
+++ src/app/updatechart/updatechart.page.ts
@@ -6,6 +6,7 @@ const subject = webSocket("wss://ws.finnhub.io?token=c5g1l4qad3i9cg8uch10");
 import { DatePipe } from '@angular/common';
 
 
+
 @Component({
   selector: 'app-updatechart',
   templateUrl: './updatechart.page.html',
@@ -17,6 +18,7 @@ export class UpdatechartPage implements OnInit {
   datapoints = [];
   yval:any;
   latestprice:any;
+  x:any;
   constructor(private datePipe:DatePipe) { }
 
   ngOnInit() {
@@ -43,31 +45,45 @@ export class UpdatechartPage implements OnInit {
     subject.next({ 'type': 'subscribe', 'symbol': 'COINBASE:BTC-USD' });
     
     //console.log(this.yval);
-    let dataPoints = [];
+    var minutesToAdd=3;
+    var currentDate = new Date();
+    var futureDate = new Date(currentDate.getTime() + minutesToAdd*60000);
+        let dataPoints = [];
     let dataPoints1 = [];
     let dpsLength = 0;
     let chart = new CanvasJS.Chart("chartContainer",{
       exportEnabled: true,
       zoomEnabled:true,
+      //backgroundColor: "#172331",
       title:{
         text:"BTC-USD"
       },
-      width: 900,
+      //width: 1200,
+      axisX:{
+        gridThickness: 1,
+        gridColor: "grey",
+        minimum: 0,
+        interval:0.5
+        // stripLines:[
+        //   {
+              
+        //       value:50,                
+        //       color:"red",
+    
+        //   }
+        //   ]
+        //interval:0.25
+      },
       axisY:{
-       interval:50,
-       crosshair: {
+        interval:50,
+        gridThickness: 1,
+        gridColor: "grey",
+        crosshair: {
         enabled: true,
         snapToDataPoint: true,
         valueFormatString: "#,##0.##",
-        // labelFormatter: function (e) {
-				// 	return "Y-Value is " + CanvasJS.formatNumber(e.value, "#,###");
-				// }
       }
-// minimum: 57000,
-// maximum: 58000,
-// interval: 100,
-// valueFormatString: "#",
-//   interval: 1
+
  },
       data: [{
         type: "line",
@@ -75,44 +91,38 @@ export class UpdatechartPage implements OnInit {
       }
     ]
     });
-    //console.log("outside if");
     setInterval(()=>{
       if(this.datapoints.length!=0){
-      // if(dataPoints.length==10){
-      //   dataPoints.splice(10,dataPoints.length-10);
-      // }
       console.log(dataPoints);
-      //console.log("inside if");
       var getdata = JSON.parse(sessionStorage.getItem('socketdata'));
       var latestdata = getdata[getdata.length-1].y;
           var d = new Date();
           var datetext = d.toTimeString();
           datetext = datetext.split(' ')[0];
-          dataPoints.push({label:datetext,y:latestdata});
-          if(dataPoints.length==100){
-            dataPoints.splice(100,dataPoints.length-100);
+          if(dataPoints.length==0){
+            dataPoints.push({label:datetext,y:latestdata},{label:"extra",y:null})
+          }
+          else{
+            dataPoints.splice(dataPoints.length-1,0,{label:datetext,y:latestdata});
+            dataPoints[dataPoints.length-1].x = dataPoints.length-1;
+          }
+          //this.x = 20
+          if(dataPoints.length==50){
+            dataPoints.shift();
+            for(var j=0;j<dataPoints.length;j++){
+              dataPoints[j].x = j;
+              //this.x=j;
+            }
+            // dataPoints[dataPoints.length-1].x = dataPoints.length-1;
           }
           console.log(dataPoints);
           chart.render();
+          //var interval = chart.axisX[0].get("interval");
+          // console.log(interval);
+          //chart.axisX[0].set("interval", 0.5)
           chart.axisY[0].crosshair.showAt(latestdata);
-      //chart.axisX[0].crosshair.set("labelFormatter",latestdata);
-      //console.log(getdata);
-      // $.getJSON("https://canvasjs.com/services/data/datapoints.php?ystart="+getdata[0].y+"&type=json&callback=?", function(data) {  
-      //   //console.log(data);
-      //   $.each(data, function(key, value){
-      //     //console.log(value);
-      //     var d = new Date();
-      //     var datetext = d.toTimeString();
-      //     datetext = datetext.split(' ')[0];
-      //     dataPoints.push({label: datetext, y: value[1]/100});
-      //   });
-      //   console.log(dataPoints[dataPoints.length-1].y);
-      //   dpsLength = dataPoints.length;
-      //   if(dpsLength>1){
-      //     dataPoints.shift();
-      //   }
-      //   chart.render();
-      // });
+          //chart.axisX[0].crosshair.showAt(x);
+          
     }
     },1000)
 }
